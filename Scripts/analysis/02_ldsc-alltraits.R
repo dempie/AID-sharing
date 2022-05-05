8bb222d1c40311514204dcceac11c93af248bff3
Pietro Demela (ext) <pietro.demela@fht.org>
2022-05-06 00:48:21 +0200

    Psoriasis and Atopic Derma added to 02_ldsc

--- Scripts/analysis/02_ldsc-alltraits.R
+++ Scripts/analysis/02_ldsc-alltraits.R
@@ -37,23 +37,24 @@ traits <- c('outputs/version3/01_output_prepare-sumstats/Munged-Sumstats/allergi
             'outputs/version3/01_output_prepare-sumstats/Munged-Sumstats/thyro.sumstats.gz',
             'outputs/version3/01_output_prepare-sumstats/Munged-Sumstats/uc.sumstats.gz', 
             
-            'outputs/version3/01_output_prepare-sumstats/Munged-Sumstats/t1d_chiou-2021.sumstats.gz' 
-           # 'outputs/version3/01_output_prepare-sumstats/Munged-Sumstats/derm'
+            'outputs/version3/01_output_prepare-sumstats/Munged-Sumstats/t1d_chiou-2021.sumstats.gz' ,
+            'outputs/version3/01_output_prepare-sumstats/Munged-Sumstats/derma_sliz-2021.sumstats.gz',
+            'outputs/version3/01_output_prepare-sumstats/Munged-Sumstats/psoriasis_sliz-2021.sumstats.gz'
             ) 
 
 trait.names <- c( 'allergies','ms_2','alzheimer_1', 'alzheimer_2', 'armfat', 'asthma_1', 'asthma_2', 
                  'celiac', 'crohn', 'jia', 'ms_1', 'pbc',  
                  'psc', 'ra', 'sle', 'thyro', 'uc', 
-                 't1d')
+                 't1d', 'derma', 'psoriasis')
 sample.prev <- round( c(.5, .5, .5, .5, NA, .5, (64538/(64538 + 239321)),
                         .5, .5, (3305/(3305 + 9196)), (9772 /(9772 + 16849)), .5, 
                         ( 2871 /(2871 + 12019)), .5, .5, .5, .5, 
-                        .5)
+                        .5, .5, .5)
                      ,2)
 population.prev <-  round(c((0.20 ),(35.9/100000), (0.058), (0.058), (NA), (0.0357), (0.0357), 
                             (0.014), (100/100000), (44.7/100000), (35.9/100000), (10/100000),
                             (5/100000), (460/100000), (50/100000), (0.05), (30/100000), 
-                            (0.095)
+                            (0.095), (0.15), (0.02)
                             ),5)
 
 ld <- "ldscores/eur_w_ld_chr"
@@ -63,8 +64,8 @@ wld <- "ldscores/eur_w_ld_chr"
 LDS_output <- ldsc(traits, sample.prev, population.prev, ld, wld, trait.names, stand = T)
 
 #save the output 
-saveRDS(LDS_output, file = 'outputs/version3/02_output_ldsc-all-traits/LDS_output_final')
-output2 <- readRDS('outputs/version3/02_output_ldsc-all-traits/LDS_output_final')
+saveRDS(LDS_output, file = 'outputs/version3/02_output_ldsc-all-traits/LDS_output_complete')
+output2 <- readRDS('outputs/version3/02_output_ldsc-all-traits/LDS_output_complete')
 
 #---- heritability -------------------------------------------------------------
 cbind(colnames(output2$S_Stand), (diag(output2$S)) )
