5efec7f51fb6ebf77daa2c446e755a0ce8af3d68
Pietro Demela (ext) <pietro.demela@fht.org>
2022-05-13 01:57:23 +0200

    Sbatch GWAS uc-cd-psc-ra-sle-t1d-jia-asthma-derma

--- Scripts/analysis/sbatch_scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/gwas_uc_cd_psc_ra_sle_t1d_jia_asthma_derma.script.R
+++ Scripts/analysis/sbatch_scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/gwas_uc_cd_psc_ra_sle_t1d_jia_asthma_derma.script.R
@@ -33,12 +33,12 @@ split_sum_stats <- function(summary_stats, chunk_size, which_chunk) {
 library(GenomicSEM)
 library(data.table)
 
-aid_sumstats <- readRDS('outputs/version3/04_output_sumstats-function/')
-ldsc_model <- readRDS('outputs/version3/04_output_sumstats-function/')
+aid_sumstats <- readRDS('/project/aid_sharing/AID_sharing/outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstat_outputs/sumstats_output/sumstats_uc-cd-psc-ra-sle-t1d-jia-asthma-derma.RDS')
+ldsc_model <- readRDS('/project/aid_sharing/AID_sharing/outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstat_outputs/ldsc_output/ldsc_uc-cd-psc-ra-sle-t1d-jia-asthma-derma.RDS')
 
-aid_model <-'F1 =~ NA*crohn + uc  + psc  
-F2 =~ NA*jia + sle + ra_eu + t1d 
-F3 =~ NA*asthma_2 + derma 
+aid_model <- 'F1 =~ NA*crohn + uc  + psc  
+F2 =~ NA*jia + sle + ra+ t1d 
+F3 =~ NA*asthma+ derma 
 
 F1~~F2
 F1~~F3
@@ -46,23 +46,27 @@ F2~~F3
 F1 ~~ 1*F1
 F2 ~~ 1*F2
 F3 ~~ 1*F3
-derma~~a*derma
-a>0.001
-'
 
-#dim(aid_sumstats)
-#how_many_chunks(aid_sumstats, 10000) #432
+F1 ~ SNP
+F2 ~ SNP
+F3 ~ SNP
+
+derma~~a*derma
+a>0.001'
+
+
+#dim(aid_sumstats) 3344158      24
+#how_many_chunks(aid_sumstats, 10000) #335
 
 chunk_to_use <- split_sum_stats(aid_sumstats, 10000, counter )
 
 output <- userGWAS(covstruc = ldsc_model, 
          SNPs = chunk_to_use, 
-         model = model_ok, 
+         model = aid_model, 
          sub = c("F1~SNP", "F2~SNP", 'F3~SNP') )
 
 
-saveRDS(output, file.path('outputs/version3/05_GWAS_results/GWAS_10-05-2022/chunks/', paste0(counter, '_gwas_10-05-22.RDS')))
-
+saveRDS(output, file.path('/project/aid_sharing/AID_sharing/outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_gwas_ouput/chunks/', paste0(counter, '_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma.RDS')))
 
 
 
