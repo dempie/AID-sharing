c61c0f9e78e63eaf673f3fc49b2f1d20a14a94e2
Pietro Demela (ext) <pietro.demela@fht.org>
2022-05-31 16:40:59 +0200

    colocalize only factors

--- Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_C_colocalization.R
+++ Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_C_colocalization.R
@@ -20,25 +20,12 @@ create_range <- function(res, chr='CHR', start='start', end='end', w=T, tag=NA )
 
 
 #--------------------------------------------------------------------------------
-sstats_names <- list.files('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/munged/')
-sstats_names  <- sstats_names[c(-8, -9, -12)]
-the_paths<- as.list(paste0('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/munged/', sstats_names))
-
-#loop for loading all the files, create a vector of names and find the SNPs that are present in all the GWAS
-gwas_names <- list()
-
-
-for( i in c(1:length(sstats_names ))){
-  #createa lit of trait names
-  gwas_names[i] <- strsplit(sstats_names , '_')[[i]][[1]]
-}
-
 
 #this function identifies significant loci with the locus.breaker fuction
 #then creates a table of all the loci for all traits and puts them togehter.
 #then looks at the overlapps between the loci and creates a macro loci that do not overlapp among them and assigns them a unique number (column pan_loci)
 #the function requires the sumstatsto have the columns: SNP, CHR,BP, A2 (EFFECT_ALLELE), A1 (NON_EFFECT_ALLELE), BETA,SE, P
-
+#restar R to clear memory before and after running locus_lister, as it sometimes too large objects are generated
 locus_lister <- function(my_paths, gwas_names) {
   # require(data.table)
   # require(dplyr)
@@ -82,83 +69,11 @@ locus_lister <- function(my_paths, gwas_names) {
   
 }
 
-#337
 
-#---------generate the list of all loci------------------------------------------
-
-loci <- locus_lister(the_paths[c(1,2, 3)], gwas_names = gwas_names[c(1,2,3)] )
-head(loci)
-dim(loci) #573 12
-
-fwrite(loci, 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/all_loci_all_traits.txt', sep = '\t', col.names = T, row.names = F, quote = F)
-saveRDS(shared_SNPs, 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_all_against_all/shared_SNPs_all_traits.RDS')
-
-  
-
-#start by searching the commong SNPs between all the gwas and generate a referenc set 
-#shared_SNPs <- Reduce(intersect, SNPs)  #shared SNPs among all the GWAS
-reference_file <- fread('SNP/reference.1000G.maf.0.005.txt.gz', data.table = F) #load the reference file
-ref_set <- reference_file[reference_file$SNP   %in%  shared_SNPs, ] #create a reference set of the positions of the shared SNPs
-loc_index <- sort(unique(loci$pan_locus))
-
-betas <- list()
-SE <- list()
-res_coloc <- list()
-
-for(i in c(1:length(unique(loci$pan_locus)))){
-  
-        #select the active locus, the beginning and the end of the locus
-        start_loc <- min(loci[loci$pan_locus==loc_index[i], 'start'] )
-        end_loc <- max(loci[loci$pan_locus==loc_index[i], 'end'] )
-        chr_loc <- unique(loci[loci$pan_locus==loc_index[i], 'chr'])
-
-        #the active SNP for locus i
-        SNP_active <- ref_set[c( between(ref_set$BP,  start_loc ,  end_loc)  & ref_set$CHR== chr_loc ), ]$SNP
-        
-      
-        beta_locus <- data.frame(row.names =SNP_active )
-        se_locus <- data.frame(row.names =SNP_active )
-        #             
-                    
-                          #for each GWAS take out the BETAs and the SE
-                          for(k in c(1:length(unlist( loci[loci$pan_locus==i,]$trait)))){
-                                    #take only the ones that are significant fot that loci
-                                    tt<- unlist( loci[loci$pan_locus==i,]$trait)[k] 
-                                    tr_SNP_active <- list_of_files[[tt]][ list_of_files[[tt]]$SNP %in% SNP_active ,]
-                                    tr_SNP_active <- tr_SNP_active[!duplicated(tr_SNP_active ), ] #remove duplicated SNPs 
-                                    #exctract the betas and se
-                                    beta_locus[, paste0(tt,'-',k,'_locus', i)] <- select(tr_SNP_active, 'BETA')
-                                    se_locus[, paste0(tt,'-',k,'_locus', i)] <- select(tr_SNP_active, 'SE')
-                                  
-                          }
-                    
-        
-        #run coloc only if the number of traits for that locus is more than one (otherwise you will have an error) and if there are at least 10 SNPs
-        if(ncol(beta_locus)>1 & nrow(beta_locus)>10){
-          traits <- colnames(beta_locus)
-          rsid <- rownames(beta_locus)
-          res_coloc[[paste0('locus_', i)]] <- hyprcoloc( effect.est = as.matrix(beta_locus), effect.se = as.matrix(se_locus), trait.names=traits, snp.id=rsid)
-
-        }
-
-        betas[[paste0('locus_', i)]] <- beta_locus
-        SE[[paste0('locus_', i)]] <- se_locus
-             
-        print(i)
-     
-}      
-
-length(res_coloc)
-
-saveRDS(res_coloc, 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_all_against_all/hyprcoloc_all_against_all_outputs.RDS')
-saveRDS(betas,'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_all_against_all/betas_all_against_all.RDS')
-saveRDS(SE,'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_all_against_all/SE_all_against_all.RDS')
-
-
-
-
-#-------------RUN hyprcoloc only on the three factors ---------------------------
-    
+#-------------function for colocalization -------------------------------------
+#this function takes as input a list of paths of munged summmary stats, thier respective names in list format, the output from locus_breaker. 
+#it returns an object with the betas and se involved in each of the loci (the loci argument), the outputs from colocalization and how many snps are involved for each locus. 
+#restar R to clear memory before and after running colocalize, as it sometimes too large objects are generated
 colocalize <- function(list_of_paths, trait_names, loci){
     #packages 
     # require(data.table)
@@ -247,19 +162,44 @@ colocalize <- function(list_of_paths, trait_names, loci){
 }
 
 
+
+#----------- run the function---------------------------------------------------
+
+#gather te info about the paths and the gwas names
+sstats_names <- list.files('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/munged/')
+sstats_names  <- sstats_names[c(-8, -9, -12)]
+the_paths<- as.list(paste0('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/munged/', sstats_names))
+
+#loop for loading all the files, create a vector of names and find the SNPs that are present in all the GWAS
+gwas_names <- list()
+
+
+for( i in c(1:length(sstats_names ))){
+  #createa lit of trait names
+  gwas_names[i] <- strsplit(sstats_names , '_')[[i]][[1]]
+}
+
+
 #--------- all against all -----------------------------------------------------
 
 all_coli <- locus_lister(the_paths, gwas_names)
+fwrite(all_coli, 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_all_against_all/loci_all_traits.txt')
 head(all_coli)
 dim(all_coli) #573  12
 
-all_coloc <- colocalize(list_of_paths = the_paths, trait_names =  gwas_names ,loci = all_coli)
+all_coli<- fread('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_all_against_all/loci_all_traits.txt',data.table = F)
+#restar R to clear memory before and after running, as it sometimes too large objects are generated
 
-saveRDS(all_coloc, 'out')
+
+all_coloc <- colocalize(list_of_paths = the_paths, trait_names =  gwas_names ,loci = all_coli)
+saveRDS(all_coloc, 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_all_against_all/colocalize_all_loci_all_traits.RDS')
+
 #------only factors ------------------------------------------------------------
 
-factor_loci 
-factor_coloc
+factor_loci <- locus_lister(the_paths[c(4,5,6)], gwas_names[c(4,5,6)])
+fwrite(factor_loci , 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_only_factors/factor_loci.txt', sep = '\t', col.names = T, row.names = F, quote = F)
+factor_coloc <- colocalize(list_of_paths = the_paths[c(4,5,6)], trait_names = gwas_names[c(4,5,6)],loci = factor_loci)
+saveRDS(factor_coloc, 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/07_colocalization/hyprcoloc_only_factors/colocalize_factors.RDS')
 
 
 
