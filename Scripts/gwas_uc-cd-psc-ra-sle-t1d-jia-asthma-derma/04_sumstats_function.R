2119ea0a77de7c642489d2b2ee1e73e4bcdd818f
Pietro Demela (ext) <pietro.demela@fht.org>
2022-06-20 18:24:29 +0200

    correlation matrix

--- Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstats_function.R
+++ Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstats_function.R
@@ -12,9 +12,7 @@
 
 #This means that I have to run again the analysis from the munge step for only 
 #the GWAS we are keeping in the model. 
-#The GWAS included in the three factor model are cuc-cd-psc-ra-sle-t1d-jia-asthma-derm
-#as in step3
-
+#The GWAS included in the three factor model are cuc-cd-psc-ra-sle-t1d-jia-asthma-derma
 
 #--- Load the libraries--------------------------------------
 library(data.table)
@@ -22,6 +20,8 @@ library(GenomicSEM)
 library(tidyr)
 library(dplyr)
 library(corrplot)
+library(extrafont)
+loadfonts()
 
 #----CHeck the standard errors--------------------------------------------------
 
@@ -180,11 +180,29 @@ system('mv *log outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstat_ou
 saveRDS(ldsc_output, 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstat_outputs/ldsc_output/ldsc_uc-cd-psc-ra-sle-t1d-jia-asthma-derma.RDS')
 ldsc_ok <- readRDS('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstat_outputs/ldsc_output/ldsc_uc-cd-psc-ra-sle-t1d-jia-asthma-derma.RDS')
 
-rownames(ldsc_ok$S_Stand) <- colnames(ldsc_ok$S_Stand)
-pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstat_outputs/correlation_matrix_uc-cd-psc-ra-sle-t1d-jia-asthma-derma.pdf', height = 16, width = 16)
-corrplot(ldsc_ok$S_Stand, order = 'hclust', addCoef.col = 'black', type = 'upper', is.corr = T)
+
+
+#plot the genetic correlation matrix
+names_plot = c('Type 1 Diabetes','Crohn\'s disease', 'Ulcerative colitis', 
+               'Primary sclerosing cholangitis ', 'Juvenile idiopathic arthritis', 'Systemic lupus erythematosus', 
+               'Rheumatoid arthritis',  'Asthma', 'Atopic dermatitis')
+rownames(ldsc_ok$S_Stand) <- names_plot
+colnames(ldsc_ok$S_Stand) <- names_plot
+pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/04_sumstat_outputs/correlation_matrix_uc-cd-psc-ra-sle-t1d-jia-asthma-derma.pdf', height = 14, width = 14)
+corrplot(ldsc_ok$S_Stand, order = 'hclust',addCoef.col = 'black', method = 'square',type = 'upper', is.corr = T,
+        tl.col='black', outline=F,
+        number.cex= 1.5,
+         tl.cex=1.5,
+        cl.cex=1.5,
+         tl.srt= 45,
+         addgrid.col='grey', 
+         family="Helvetica",
+        title= 'LDSC genetic correlation matrix of autoimmune diseases', mar=c(0,0,1,0)
+         )
+
 dev.off()
 
+?corrplot
 
 #---- Two factor model ---------------------------------------------------------
 aid_model <-'F1 =~ NA*crohn + uc  + psc  
