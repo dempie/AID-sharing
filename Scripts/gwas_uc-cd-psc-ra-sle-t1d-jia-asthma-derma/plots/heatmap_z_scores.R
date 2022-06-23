b4ca2404bf615e77e7eaa6e5b67cb9ee17a2ebbd
Pietro Demela (ext) <pietro.demela@fht.org>
2022-06-23 14:36:31 +0200

    heatmap factro only

--- Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/heatmap_z_scores.R
+++ Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/heatmap_z_scores.R
@@ -1,15 +1,6 @@
 library(data.table)
-library(ChIPpeakAnno)
 library(stringr)
-library(gprofiler2)
-library(GenomicRanges)
-library(EnsDb.Hsapiens.v75)
-library(biomaRt)
 library(ComplexHeatmap)
-library(clusterProfiler)
-library(enrichplot)
-library(GOSemSim)
-library(gtools)
 library(RColorBrewer)
 library(circlize)
 
@@ -27,34 +18,115 @@ for(i in 1:3){
 
 all_f_s<- do.call(rbind, f_snps)
 all_f_s$z_score <- all_f_s$BETA/all_f_s$SE
-to_plot <- data.frame(row.names =all_f_s[all_f_s$LHS=='F1', ]$SNP, f1= all_f_s[all_f_s$LHS=='F1', ]$z_score, f2=all_f_s[all_f_s$LHS=='F2', ]$z_score, f3=all_f_s[all_f_s$LHS=='F3', ]$z_score )
+to_plot <- data.frame(row.names = unique(factor_loci$pan_locus_name), 
+                      F1=rep(NA, length(unique(factor_loci$pan_locus_name))),  
+                      F2=rep(NA, length(unique(factor_loci$pan_locus_name))),
+                      F3=rep(NA, length(unique(factor_loci$pan_locus_name))), 
+                      traits=rep(NA, length(unique(factor_loci$pan_locus_name))),
+                      leads=rep(NA, length(unique(factor_loci$pan_locus_name))))
 
-to_plot$type <- rep(NA, nrow(to_plot))
+
+
 for(i in 1:nrow(to_plot)){
-  to_plot[i,]$type <-paste0(unique(factor_loci[factor_loci$pan_locus==factor_loci[factor_loci$SNP==rownames(to_plot[i,]),]$pan_locus, ]$trait), collapse='-')
+  tt <- rownames(to_plot)[i]
+  to_plot[tt, ]$traits <- paste0(factor_loci[factor_loci$pan_locus_name== tt,]$trait, collapse = '-')
+  to_plot[tt, ]$leads <- paste0(factor_loci[factor_loci$pan_locus_name== tt, ]$SNP, collapse = '-')
+  
 }
 
 
+mean(to_plot[to_plot$traits %in% c('f1', 'f2', 'f3'),]$leads %in% all_f_s$SNP) #all SNP are present in the factor specifics group
 
-pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/heatmap_complete_absolute_z_scores_all_factors.pdf', height = 16, width = 8)
-Heatmap(as.matrix(abs(to_plot[,1:3])),
+
+#generate a list of factor specific zetas
+to_plot_f <- to_plot[to_plot$traits %in% c('f1', 'f2', 'f3'),]
+for(i in 1:3){
+      tt <- c('f1','f2', 'f3')[i]
+     
+      for(k in 1:nrow(to_plot_f)){
+            snp <- to_plot_f[k,]$leads
+            to_plot_f[to_plot_f$leads==snp, toupper(tt)]  <-   all_f_s[all_f_s$LHS==toupper(tt) & all_f_s$SNP==snp,]$z_score
+      }
+  
+}
+
+
+#----------- z scores only factor specific -------------------------------------
+RColorBrewer::display.brewer.all(colorblindFriendly = T)
+
+
+pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/figure1/heatmap_complete_absolute_z_scores_specifc_factors.pdf', height = 16, width = 6)
+Heatmap(as.matrix(abs(to_plot_f[,1:3])),
         col = colorRamp2(c(seq(1,13, length.out=9)), c(RColorBrewer::brewer.pal(9,'Blues'))), 
         column_title = "Heatmap of absolute z scores" , 
         cluster_rows = T,
         cluster_columns = F,
-        cluster_column_slices = T,
+        cluster_column_slices = F,
         cluster_row_slices =  F,
         show_row_dend = F,
-        row_split = factor(to_plot[,4], levels = c('f1', 'f2', 'f3', 'f1-f2', 'f1-f3', 'f2-f3', 'f1-f2-f3' )),
+        row_split = factor(toupper(to_plot_f[,'traits']), levels = c('F1', 'F2', 'F3')),
         column_gap = unit(3, "mm"),
         row_gap = unit(5, "mm") ,
         border = T,
         row_names_gp = gpar( fontsize=0), 
+       # row_title='Lead SNP',
+        heatmap_legend_param =list(title = "Absosule z score"))
+dev.off()
+?Heatmap
+
+#----------- z scores only factor shared-- -------------------------------------
+
+RColorBrewer::display.brewer.all(colorblindFriendly = T)
+
+to_pl_f_shared <- to_plot[!to_plot$TYPE %in% c('F1', 'F2', 'F3'),]
+
+#pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/figure1/heatmap_complete_absolute_z_scores_overlapping_factors.pdf', height = 16, width = 6)
+Heatmap(as.matrix(abs(to_pl_f_shared[,1:3])),
+        col = colorRamp2(c(seq(1,13, length.out=9)), c(RColorBrewer::brewer.pal(9,'Blues'))), 
+        column_title = "Heatmap of absolute z scores" , 
+        cluster_rows = F,
+        cluster_columns = F,
+        cluster_column_slices = T,
+        cluster_row_slices =  F,
+        show_row_dend = F,
+        row_split = factor(to_pl_f_shared[,4], levels = c('F1-F2', 'F1-F3', 'F2-F3', 'F1-F2-F3' )),
+        column_gap = unit(3, "mm"),
+        row_gap = unit(5, "mm") ,
+        border = T,
+        row_names_gp = gpar( fontsize=10), 
         heatmap_legend_param =list(title = ""))
 dev.off()
 
 
-RColorBrewer::display.brewer.all(colorblindFriendly = T)
+#plot the lead SNPs for that locus, and call locus by name
+to_plot$pan_locus <- rep(NA, nrow(to_plot))
+to_pl_f_shared <- to_plot[!to_plot$TYPE %in% c('F1', 'F2', 'F3'),]
+
+
+for(i in 1:nrow(to_pl_f_shared)){
+to_pl_f_shared$pan_locus[i] <- factor_loci[factor_loci$SNP %in% rownames(to_pl_f_shared)[i],]$pan_locus_name
+}
+
+
+to_pl_f_shared
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
