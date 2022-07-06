d8b23db069bd0c460b40f41028586d66bbd01da9
Pietro Demela (ext) <pietro.demela@fht.org>
2022-07-06 14:46:42 +0200

    heatmqp pathways script

---
+++ Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/heatmap_pathways.R
@@ -0,0 +1,206 @@
+library(data.table)
+library(ChIPpeakAnno)
+library(stringr)
+library(gprofiler2)
+library(GenomicRanges)
+library(EnsDb.Hsapiens.v75)
+library(biomaRt)
+library(ComplexHeatmap)
+library(gtools)
+library(RColorBrewer)
+
+mart <- useDataset("hsapiens_gene_ensembl", useMart(biomart="ENSEMBL_MART_ENSEMBL", host="https://grch37.ensembl.org", path="/biomart/martservice" ,dataset="hsapiens_gene_ensembl"))
+loci.table <- fread('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/08_genes_and_pathways_conditional/loci_table_names_nicola_genes.csv', data.table = F)
+
+kegg <- readRDS('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/08_genes_and_pathways_conditional/kegg_pathway_analysis.RDS')
+go <- readRDS('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/08_genes_and_pathways_conditional/go_pathway_analysis.RDS')
+
+#-----heatmap kegg results------------------------------------------------------
+
+he <- kegg$result
+genes <- unique(strsplit(paste0(he$intersection, collapse = ','), split = ',')[[1]])
+genes_symbol <- getBM(filters= "ensembl_gene_id", attributes= c("entrezgene_id","ensembl_gene_id", 'hgnc_symbol'),values=  genes,mart=mart )
+
+genes
+t_t <- data.frame(genes) 
+for(i in 1:length(he$term_name)){
+  t_t[, paste0(paste(strsplit(he$term_name[i], split=' ')[[1]], collapse = '_'),'_' , he$query[i])] <- as.numeric(genes %in% strsplit(he[he$term_name==he$term_name[i] & he$query==he$query[i], 'intersection'], split=',' )[[1]])
+  
+}
+
+t_t
+#add gene symbol and remove ENSEMBLE
+rownames(t_t) <- genes_symbol$hgnc_symbol[match(t_t$genes, genes_symbol$ensembl_gene_id)]   
+t_t$genes <- NULL
+t_t
+
+t_t['trait',] <- c(he$query) 
+t_t['p_value',] <- c(he$p_value)
+tt <- t(t_t)
+
+
+#color selection
+colori<- ComplexHeatmap:::default_col(x = tt)
+colori[c('f1', 'f2', 'f3')] <-  RColorBrewer::brewer.pal(5, 'Paired')[c(1,3,5)]#colorblind safe
+colori[c('0','1')] <- brewer.pal(9, 'Greys')[c(2,8)]
+
+#column split
+sep <- c(rep('a', nrow(t_t)-2), rep('b', 2))
+names(sep) <- colnames(tt)
+
+ht_opt$ROW_ANNO_PADDING = unit(0.2, "cm")
+#plot heatmap_gene_and_pathways.pdf             
+pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/figure_pathways/heatmap_gene_and_pathways_only_path_genes.pdf', height = 8, width = 16)
+Heatmap(tt, column_title = "Genes and pathways KEGG genes",
+        rect_gp = gpar(col = "white", lwd = 0.25, type='2'), 
+        column_title_gp = gpar(fontsize = 20, fontface = "bold"),
+        left_annotation = rowAnnotation( '-log10(Padj)' = anno_barplot(   axis_param = list(direction = "reverse"),-log10(as.numeric(tt[,'p_value'])), width = unit(2, "cm"))),
+        row_split = tt[,'trait'],
+        column_split = sep,
+        column_gap = unit(2, "mm"),
+        border=T,
+        row_gap = unit(5, "mm"), 
+        row_names_gp = gpar(col =   RColorBrewer::brewer.pal(5, 'Paired')[c(1,3,5)], fontsize=8),
+        column_labels = c(colnames(tt)[1:c(nrow(t_t)-2)], 'Factor', '') ,
+        column_names_gp = gpar(fontsize=7),
+        column_names_side = 'bottom',
+        show_heatmap_legend = F,
+        row_title = "Pathways",
+        na_col = 'red', 
+        col = colori, 
+        heatmap_width = unit(28, 'cm'), heatmap_height = unit(12, 'cm')
+)
+dev.off()
+
+
+#---- plot the heatmpat with all genes -----------------------------------------
+
+he <- kegg$result
+genes <- unique(loci.table[order(loci.table$trait),]$closest_gene)
+genes_symbol <- getBM(filters= "ensembl_gene_id", attributes= c("entrezgene_id","ensembl_gene_id", 'hgnc_symbol'),values=  genes,mart=mart )
+
+genes
+t_t <- data.frame(genes) 
+for(i in 1:length(he$term_name)){
+  t_t[, paste0(paste(strsplit(he$term_name[i], split=' ')[[1]], collapse = '_'),'_' , he$query[i])] <- as.numeric(genes %in% strsplit(he[he$term_name==he$term_name[i] & he$query==he$query[i], 'intersection'], split=',' )[[1]])
+  
+}
+
+genes_symbol[!str_detect(genes_symbol$hgnc_symbol,'' ),]$hgnc_symbol <- genes_symbol[!str_detect(genes_symbol$hgnc_symbol,'' ),]$ensembl_gene_id
+#add gene symbol and remove ENSEMBLE
+rownames(t_t) <- genes_symbol$hgnc_symbol[match(t_t$genes, genes_symbol$ensembl_gene_id)]   
+t_t$genes <- NULL
+t_t
+
+t_t['trait',] <- c(he$query) 
+t_t['p_value',] <- c(he$p_value)
+tt <- t(t_t)
+
+
+#color selection
+colori<- ComplexHeatmap:::default_col(x = tt)
+colori[c('f1', 'f2', 'f3')] <-  RColorBrewer::brewer.pal(5, 'Paired')[c(1,3,5)]#colorblind safe
+colori[c('0','1')] <- brewer.pal(9, 'Greys')[c(2,8)]
+
+#column split
+sep <- c(rep('a', nrow(t_t)-2), rep('b', 2))
+names(sep) <- colnames(tt)
+
+ht_opt$ROW_ANNO_PADDING = unit(0.2, "cm")
+#plot heatmap_gene_and_pathways.pdf         
+
+
+#plot heatmap_gene_and_pathways_with_all_genes.pdf             
+pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/figure_pathways/heatmap_gene_and_pathways_all_genes_full_matrix.pdf', height = 8, width = 16)
+Heatmap(tt, column_title = "Genes and pathways all genes KEGG",
+        rect_gp = gpar(col = "white", lwd = 0.25, type='2'), 
+        column_title_gp = gpar(fontsize = 20, fontface = "bold"),
+        left_annotation = rowAnnotation( '-log10(Padj)' = anno_barplot(   axis_param = list(direction = "reverse"),-log10(as.numeric(tt[,'p_value'])), width = unit(2, "cm"))),
+        row_split = tt[,'trait'],
+        column_split = sep,
+        column_gap = unit(2, "mm"),
+        border=T,
+        row_gap = unit(5, "mm"), 
+        row_names_gp = gpar(col =   RColorBrewer::brewer.pal(5, 'Paired')[c(1,3,5)], fontsize=8),
+        column_labels = c(colnames(tt)[1:c(nrow(t_t)-2)], 'Factor', '') ,
+        column_names_gp = gpar(fontsize=2),
+        column_names_side = 'bottom',
+        show_heatmap_legend = F,
+        row_title = "Pathways",
+        na_col = 'red', 
+        col = colori, 
+        heatmap_width = unit(30, 'cm'), heatmap_height = unit(10, 'cm')
+)
+dev.off()
+
+
+
+#-------------------heatmpap and go terms------------------------------------
+
+
+
+he <- go$result
+genes <- unique(strsplit(paste0(he$intersection, collapse = ','), split = ',')[[1]])
+genes_symbol <- getBM(filters= "ensembl_gene_id", attributes= c("entrezgene_id","ensembl_gene_id", 'hgnc_symbol'),values=  genes,mart=mart )
+
+genes
+t_t <- data.frame(genes) 
+for(i in 1:length(he$term_name)){
+  t_t[, paste0(paste(strsplit(he$term_name[i], split=' ')[[1]], collapse = '_'),'_' , he$query[i])] <- as.numeric(genes %in% strsplit(he[he$term_name==he$term_name[i] & he$query==he$query[i], 'intersection'], split=',' )[[1]])
+  
+}
+
+t_t
+#add gene symbol and remove ENSEMBLE
+rownames(t_t) <- genes_symbol$hgnc_symbol[match(t_t$genes, genes_symbol$ensembl_gene_id)]   
+t_t$genes <- NULL
+t_t
+
+t_t['trait',] <- c(he$query) 
+t_t['p_value',] <- c(he$p_value)
+tt <- t(t_t)
+
+
+#color selection
+colori<- ComplexHeatmap:::default_col(x = tt)
+colori[c('f1', 'f2', 'f3')] <-  RColorBrewer::brewer.pal(5, 'Paired')[c(1,3,5)]#colorblind safe
+colori[c('0','1')] <- brewer.pal(9, 'Greys')[c(2,8)]
+
+#column split
+sep <- c(rep('a', nrow(t_t)-2), rep('b', 2))
+names(sep) <- colnames(tt)
+
+
+
+#----plot the GO terms heatmap -------------------------------------------------
+
+pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/figure_pathways/heatmap_GO_gene_and_pathways_only_path_genes.pdf', height = 20, width = 20)
+Heatmap(tt, column_title = "Genes and pathways GO terms",
+        rect_gp = gpar(col = "white", lwd = 0.25), 
+        column_title_gp = gpar(fontsize = 20, fontface = "bold"),
+        left_annotation = rowAnnotation( '-log10(Padj)' = anno_barplot(   axis_param = list(direction = "reverse"),-log10(as.numeric(tt[,'p_value'])), width = unit(2, "cm"))),
+        row_split = tt[,'trait'],
+        border=T,
+        column_split = sep,
+        column_gap = unit(3, "mm"),
+        row_gap = unit(5, "mm"), 
+        row_names_gp = gpar(col =   RColorBrewer::brewer.pal(5, 'Paired')[c(1,3,5)], fontsize=2),
+        column_names_gp = gpar(fontsize=3),
+        column_names_side = 'bottom',
+        show_heatmap_legend = F,
+        row_title = "Pathways",
+        na_col = 'red', 
+        col = colori, 
+        heatmap_width = unit(30, 'cm'), heatmap_height = unit(30, 'cm')
+)
+dev.off()
+
+
+head(go$result[order(go$result$p_value), c('query', 'p_value', 'term_name')],10)
+
+go_f <- list()
+for(i in 1:3){
+  tt <- c('f1', 'f2', 'f3')[i]
+  go_f[[tt]] <- go$result[ go$result$query==tt,]
+}
+
