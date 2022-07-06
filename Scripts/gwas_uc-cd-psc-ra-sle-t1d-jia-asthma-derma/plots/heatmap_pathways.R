6254e243e39a32619814a4e655f06b7e7ab4d88b
Pietro Demela (ext) <pietro.demela@fht.org>
2022-07-06 18:06:36 +0200

    adjustments

--- Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/heatmap_pathways.R
+++ Scripts/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/heatmap_pathways.R
@@ -8,6 +8,7 @@ library(biomaRt)
 library(ComplexHeatmap)
 library(gtools)
 library(RColorBrewer)
+library(formattable)
 
 mart <- useDataset("hsapiens_gene_ensembl", useMart(biomart="ENSEMBL_MART_ENSEMBL", host="https://grch37.ensembl.org", path="/biomart/martservice" ,dataset="hsapiens_gene_ensembl"))
 loci.table <- fread('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/08_genes_and_pathways_conditional/loci_table_names_nicola_genes.csv', data.table = F)
@@ -110,7 +111,15 @@ ht_opt$ROW_ANNO_PADDING = unit(0.2, "cm")
 #plot heatmap_gene_and_pathways.pdf         
 
 
-#plot heatmap_gene_and_pathways_with_all_genes.pdf             
+#plot heatmap_gene_and_pathways_with_all_genes.pdf
+
+for(i in 1:nrow(tt)){
+    rownames(tt)[i] <- str_replace(string = rownames(tt)[i],pattern = '_', replacement = ' ' )
+    rownames(tt)[i] <- str_remove(rownames(tt)[i], pattern = '_f1')
+    rownames(tt)[i] <- str_remove(rownames(tt)[i], pattern = '_f2')
+    rownames(tt)[i] <- str_remove(rownames(tt)[i], pattern = '_f3')
+}
+
 pdf('outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/figure_pathways/heatmap_gene_and_pathways_all_genes_full_matrix.pdf', height = 8, width = 16)
 Heatmap(tt, column_title = "Genes and pathways all genes KEGG",
         rect_gp = gpar(col = "white", lwd = 0.25, type='2'), 
@@ -121,15 +130,15 @@ Heatmap(tt, column_title = "Genes and pathways all genes KEGG",
         column_gap = unit(2, "mm"),
         border=T,
         row_gap = unit(5, "mm"), 
-        row_names_gp = gpar(col =   RColorBrewer::brewer.pal(5, 'Paired')[c(1,3,5)], fontsize=8),
+        row_names_gp = gpar(col =   RColorBrewer::brewer.pal(5, 'Paired')[c(1,3,5)], fontsize=10),
         column_labels = c(colnames(tt)[1:c(nrow(t_t)-2)], 'Factor', '') ,
-        column_names_gp = gpar(fontsize=2),
+        column_names_gp = gpar(fontsize=0),
         column_names_side = 'bottom',
         show_heatmap_legend = F,
         row_title = "Pathways",
         na_col = 'red', 
         col = colori, 
-        heatmap_width = unit(30, 'cm'), heatmap_height = unit(10, 'cm')
+        heatmap_width = unit(30, 'cm'), heatmap_height = unit(14, 'cm')
 )
 dev.off()
 
@@ -203,4 +212,58 @@ for(i in 1:3){
   tt <- c('f1', 'f2', 'f3')[i]
   go_f[[tt]] <- go$result[ go$result$query==tt,]
 }
+
+
+
+#-------------------table of pathways-------------------------------------------
+
+#kegg table
+kg <- kegg$result[, c('query','p_value', 'term_name', 'intersection')] 
+
+#use gene symbols
+he <- kegg$result
+genes <- unique(strsplit(paste0(he$intersection, collapse = ','), split = ',')[[1]])
+genes_symbol <- getBM(filters= "ensembl_gene_id", attributes= c("entrezgene_id","ensembl_gene_id", 'hgnc_symbol'),values=  genes,mart=mart )
+
+kg$hgnc <- rep('NA', nrow(kg))
+for(i in 1:nrow(kg)){
+  kg$hgnc[i] <- paste0(genes_symbol[ genes_symbol$ensembl_gene_id %in% strsplit( kg$intersection[i], split = ',')[[1]] ,]$hgnc_symbol, collapse = ',  ')
+}
+
+colnames(kg) <- c('Trait', 'P-value (adjusted)', 'Pathway', 'ENSEMBL', 'Gene symbols')
+kg$Trait <- toupper(kg$Trait)
+plott <- kg[,c(3,1,2, 5)]
+
+
+
+formattable(plott,align =c("l","c","c" ,"r") ,
+            list( 'Pathway' = formatter( "span", style = style(color = "grey",font.weight = "bold")))
+)
+
+fwrite(plott, file = 'outputs/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/plots/figure_pathways/kegg_results.csv',sep = ',', col.names = T, quote = F, row.names = F)
+
+#go table
+
+gg <-  go$result[, c('query','p_value', 'term_name', 'intersection')] 
+gg <- head(gg[order(gg$p_value), ],20)
+genes <- unique(strsplit(paste0(gg$intersection, collapse = ','), split = ',')[[1]])
+genes_symbol <- getBM(filters= "ensembl_gene_id", attributes= c("entrezgene_id","ensembl_gene_id", 'hgnc_symbol'),values=  genes,mart=mart )
+gg$hgnc <- rep('NA', nrow(gg))
+
+for(i in 1:nrow(gg)){
+  gg$hgnc[i] <- paste0(genes_symbol[ genes_symbol$ensembl_gene_id %in% strsplit( gg$intersection[i], split = ',')[[1]] ,]$hgnc_symbol, collapse = ',  ')
+}
+
+colnames(gg) <- c('Trait', 'P-value (adjusted)', 'GO term', 'ENSEMBL', 'Gene symbols')
+gg$Trait <- toupper(gg$Trait)
+plott <- gg[,c(3,1,2, 5)]
+formattable(plott,align =c("l","c","c" ,"r") ,
+            list( 'GO term' = formatter( "span", style = style(color = "grey",font.weight = "bold")))
+)
+
+
+
+
+
+
 
