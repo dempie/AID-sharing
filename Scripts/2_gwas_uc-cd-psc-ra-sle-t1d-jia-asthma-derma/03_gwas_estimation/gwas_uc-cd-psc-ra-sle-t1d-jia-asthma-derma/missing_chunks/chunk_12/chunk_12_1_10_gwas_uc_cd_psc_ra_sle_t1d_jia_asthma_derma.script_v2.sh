5a4fca4b477cc5fbcc9eeb900b9b991f11f8e972
Pietro Demela (ext) <pietro.demela@fht.org>
2022-09-07 17:03:49 +0200

    missing chunks sbatch

---
+++ Scripts/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/03_gwas_estimation/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/missing_chunks/chunk_12/chunk_12_1_10_gwas_uc_cd_psc_ra_sle_t1d_jia_asthma_derma.script_v2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name=gwas_12
+#SBATCH --partition=cpuq
+#SBATCH --array=1-10
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH --output=gwas_chunk_203_1-10-%A-%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=pietro.demela@fht.org
+#SBATCH --mem=32000
+
+
+echo '----------------------------'
+echo ' JOB ID: '$SLURM_ARRAY_JOB_ID
+echo ' CURRENT TASK ID: '$SLURM_JOB_ID
+echo ' CURRENT TASK NUMBER: '$SLURM_ARRAY_TASK_ID
+echo '----------------------------'
+echo ' MIN TASK ID: '$SLURM_ARRAY_TASK_MIN
+echo ' MAX TASK ID: '$SLURM_ARRAY_TASK_MAX
+echo ' TOTAL NUMBER OF TASKS: '$SLURM_ARRAY_TASK_COUNT
+echo '----------------------------'
+
+
+
+module load R/4.1.0
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID 
+Rscript chunk_12_1_10_gwas_uc_cd_psc_ra_sle_t1d_jia_asthma_derma.script_v2.R $SLURM_ARRAY_TASK_ID
+
