96b9b14d8b7452022b542438157d3e36d196a160
Pietro Demela (ext) <pietro.demela@fht.org>
2022-09-08 17:22:11 +0200

    assemble gwas

--- Scripts/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/03_gwas_estimation/assemble_gwas.R
+++ Scripts/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/03_gwas_estimation/assemble_gwas.R
@@ -232,7 +232,7 @@ saveRDS(N_hat,'outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_gwas_oupu
 
 
 #---plot the manatthan plot 
-jpeg(file='outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_gwas_ouput/miamiF1_F2_F3.jpeg', width = 800, height = 400, units='mm', res = 600)
+jpeg(file='outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/03_gwas_estimation/miamiF1_F2_F3.jpeg', width = 800, height = 400, units='mm', res = 600)
 par(mfrow=c(3,1))
 par(mar=c(5,5,3,3))
 manhattan(F1_plot, chr="CHR", bp="BP", snp="SNP", p="Pval_Estimate" ,ylim=c(0,30), main='F1 =~ crohn + uc  + psc', 
