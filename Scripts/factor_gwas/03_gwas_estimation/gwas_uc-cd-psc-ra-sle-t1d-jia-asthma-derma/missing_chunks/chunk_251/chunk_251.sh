82e6df1a1619f2238b462e192e18a46d3ff5ff83
Pietro Demela (ext) <pietro.demela@fht.org>
2022-12-22 09:16:13 +0100

    missing chunks script

---
+++ Scripts/factor_gwas/03_gwas_estimation/gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/missing_chunks/chunk_251/chunk_251.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name=gw_251
+#SBATCH --partition=cpuq
+#SBATCH --array=1-10
+#SBATCH --cpus-per-task=1
+#SBATCH --time=24:00:00
+#SBATCH --output=gwas_chunk_251_1-10-%A-%a.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=pietro.demela@fht.org
+#SBATCH --mem=10000
+
+
+echo '----------------------------'
+echo ' JOB ID: '$SLURM_ARRAY_JOB_ID
+echo ' CURRENT TASK ID: '$SLURM_JOB_ID
+echo ' CURRENT TASK NUMBER: '$SLURM_ARRAY_TASK_ID
+echo '----------------------------'
+echo ' MIN TASK ID: '$SLURM_ARRAY_TASK_MIN
+echo ' MAX TASK ID: '$SLURM_ARRAY_TASK_MAX
+echo ' TOTAL NUMBER OF TASKS: '$SLURM_ARRAY_TASK_COUNT
+echo '----------------------------'
+
+
+
+module load R/4.1.0
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID 
+Rscript chunk_251.R $SLURM_ARRAY_TASK_ID
+
