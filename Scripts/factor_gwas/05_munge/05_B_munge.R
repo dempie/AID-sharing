a19b249d2a520807a4e08bbb4b49782bc99e5505
Pietro Demela (ext) <pietro.demela@fht.org>
2023-01-09 10:20:49 +0100

    munge factors run

--- Scripts/factor_gwas/05_munge/05_B_munge.R
+++ Scripts/factor_gwas/05_munge/05_B_munge.R
@@ -19,89 +19,89 @@ library("BSgenome.Hsapiens.1000genomes.hs37d5")
 
 #the function will infer the genome build. I tested and manualy checked it and it is accurate. 
 #very important: the output sumstats will have the effect allele coded as A2 (a-two). . 
-
-a1 <- list.files('outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/ready_for_munge/') #get the file names
-my_paths <- as.list(paste0('outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/ready_for_munge/', a1)) #create paths
-
-
-builds <- c("GRCH37" ,"GRCH37","GRCH38" ,"GRCH37", "GRCH37" ,"GRCH37" ,"GRCH37" ,"GRCH37" ,"GRCH37" ,"GRCH37" ,"GRCH38","GRCH37" )
-
-gwas_names <- list()
-for( i in c(1:length(a1))){
-  gwas_names[i] <- strsplit(a1, '_')[[i]][[1]]
-  
-}
-
-names(my_paths) <- gwas_names #give names just for checking
-
-data('sumstatsColHeaders') #load reference file coming from the package  MungeSumstats
-output <- list()
-
-for(i in c(1:12)){
-  
-  output[[i]] <-  format_sumstats(
-    path= my_paths[[i]],
-    convert_ref_genome = 'GRCH37',
-    ref_genome = builds[i],
-    convert_small_p = F,
-    compute_z = FALSE,
-    force_new_z = FALSE,
-    compute_n = 0L,
-    convert_n_int = F,
-    analysis_trait = NULL,
-    INFO_filter = 0,
-    FRQ_filter = 0,
-    pos_se = T,   #importan for coloc
-    effect_columns_nonzero = T, #important for coloc 
-    N_std = 5,
-    N_dropNA = F,
-    rmv_chr = c("X", "Y", "MT"),
-    rmv_chrPrefix = TRUE,
-    on_ref_genome = TRUE,
-    strand_ambig_filter = FALSE,
-    allele_flip_check = TRUE,
-    allele_flip_drop = TRUE,
-    allele_flip_z = TRUE,
-    allele_flip_frq = TRUE,
-    bi_allelic_filter = TRUE,
-    snp_ids_are_rs_ids = TRUE,
-    remove_multi_rs_snp = T,     
-    frq_is_maf = TRUE,
-    sort_coordinates = TRUE,
-    nThread = 2,
-    save_path = paste0('outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/munged/', gwas_names[[i]], '_munged_build37.txt' ),
-    write_vcf = FALSE,
-    tabix_index = FALSE,
-    return_data = FALSE,
-    return_format = "data.table",
-    ldsc_format = FALSE,
-    log_folder_ind = FALSE,
-    log_folder = 'outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/log/',
-    log_mungesumstats_msgs = TRUE,
-    imputation_ind = FALSE,
-    force_new = FALSE,
-    mapping_file = sumstatsColHeaders
-  )
-  
-}
-
-
-saveRDS(output, 'outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/removed_SNP/')
+# 
+# a1 <- list.files('outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/ready_for_munge/') #get the file names
+# my_paths <- as.list(paste0('outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/ready_for_munge/', a1)) #create paths
+# 
+# 
+# builds <- c("GRCH37" ,"GRCH37","GRCH38" ,"GRCH37", "GRCH37" ,"GRCH37" ,"GRCH37" ,"GRCH37" ,"GRCH37" ,"GRCH37" ,"GRCH38","GRCH37" )
+# 
+# gwas_names <- list()
+# for( i in c(1:length(a1))){
+#   gwas_names[i] <- strsplit(a1, '_')[[i]][[1]]
+#   
+# }
+# 
+# names(my_paths) <- gwas_names #give names just for checking
+# 
+#data('sumstatsColHeaders') #load reference file coming from the package  MungeSumstats
+# output <- list()
+# 
+# for(i in c(1:12)){
+#   
+#   output[[i]] <-  format_sumstats(
+#     path= my_paths[[i]],
+#     convert_ref_genome = 'GRCH37',
+#     ref_genome = builds[i],
+#     convert_small_p = F,
+#     compute_z = FALSE,
+#     force_new_z = FALSE,
+#     compute_n = 0L,
+#     convert_n_int = F,
+#     analysis_trait = NULL,
+#     INFO_filter = 0,
+#     FRQ_filter = 0,
+#     pos_se = T,   #important for coloc
+#     effect_columns_nonzero = T, #important for coloc 
+#     N_std = 5,
+#     N_dropNA = F,
+#     rmv_chr = c("X", "Y", "MT"),
+#     rmv_chrPrefix = TRUE,
+#     on_ref_genome = TRUE,
+#     strand_ambig_filter = FALSE,
+#     allele_flip_check = TRUE,
+#     allele_flip_drop = TRUE,
+#     allele_flip_z = TRUE,
+#     allele_flip_frq = TRUE,
+#     bi_allelic_filter = TRUE,
+#     snp_ids_are_rs_ids = TRUE,
+#     remove_multi_rs_snp = T,     
+#     frq_is_maf = TRUE,
+#     sort_coordinates = TRUE,
+#     nThread = 2,
+#     save_path = paste0('outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/munged/', gwas_names[[i]], '_munged_build37.txt' ),
+#     write_vcf = FALSE,
+#     tabix_index = FALSE,
+#     return_data = FALSE,
+#     return_format = "data.table",
+#     ldsc_format = FALSE,
+#     log_folder_ind = FALSE,
+#     log_folder = 'outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/log/',
+#     log_mungesumstats_msgs = TRUE,
+#     imputation_ind = FALSE,
+#     force_new = FALSE,
+#     mapping_file = sumstatsColHeaders
+#   )
+#   
+# }
+# 
+# 
+# saveRDS(output, 'outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/removed_SNP/')
 
 
 
 
 
 ### munge olny factors
+data('sumstatsColHeaders') #load reference file coming from the package  MungeSumstats
 
-
-paths_f <- list('outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/ready_for_munge/f1_ready_for_munge.txt',
-                'outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/ready_for_munge/f2_ready_for_munge.txt',
-                'outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/ready_for_munge/f3_ready_for_munge.txt'
+paths_f <- list('outputs/rev_1/05_munge/ready_munge/f1_ready_for_munge.txt',
+                'outputs/rev_1/05_munge/ready_munge/f2_ready_for_munge.txt',
+                'outputs/rev_1/05_munge/ready_munge/f3_ready_for_munge.txt'
   
 )
 
-for(i in c(1:12)){
+for(i in c(1:3)){
   
   output[[i]] <-  format_sumstats(
     path= paths_f[[i]],
@@ -115,7 +115,7 @@ for(i in c(1:12)){
     analysis_trait = NULL,
     INFO_filter = 0,
     FRQ_filter = 0,
-    pos_se = T,   #importan for coloc
+    pos_se = T,   #important for coloc
     effect_columns_nonzero = T, #important for coloc 
     N_std = 5,
     N_dropNA = F,
@@ -133,14 +133,14 @@ for(i in c(1:12)){
     frq_is_maf = TRUE,
     sort_coordinates = TRUE,
     nThread = 2,
-    save_path = paste0('outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/munged/', c('f1', 'f2', 'f3')[i], '_munged_build37.txt' ),
+    save_path = paste0('outputs/rev_1/05_munge/munged/', c('f1', 'f2', 'f3')[i], '_munged_build37.txt' ),
     write_vcf = FALSE,
     tabix_index = FALSE,
     return_data = FALSE,
     return_format = "data.table",
     ldsc_format = FALSE,
     log_folder_ind = FALSE,
-    log_folder = 'outputs/2_gwas_uc-cd-psc-ra-sle-t1d-jia-asthma-derma/05_munge_for_nicola/log/',
+    log_folder = 'outputs/rev_1/05_munge/munged/log/',
     log_mungesumstats_msgs = TRUE,
     imputation_ind = FALSE,
     force_new = FALSE,
